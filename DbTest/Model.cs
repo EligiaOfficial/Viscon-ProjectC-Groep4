c6a495bc88d3545f372312b19e0c1f32c165c9e9
TymovanRijn <1057297@hr.nl>
2023-10-05 11:17:14 +0200

    Removed Credentials

--- DbTest/Model.cs
+++ DbTest/Model.cs
@@ -7,14 +7,14 @@ public class ApplicationDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Project_C;Username=postgres;Password=tymo2003");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Project_C;Username=;Password=");
     }
 
     public DbSet<Users> Users { get; set; }
-    public DbSet<Machines> Machines {get; set;}
+    public DbSet<Machines> Machines { get; set; }
     public DbSet<Tickets> Tickets { get; set; }
-    public DbSet<Messages> Messages {get; set;}
+    public DbSet<Messages> Messages { get; set; }
     public DbSet<Departments> Departments { get; set; }
-    
-    
+
+
 }
