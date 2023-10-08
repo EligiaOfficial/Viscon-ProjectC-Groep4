898663842679e25ab3768177dd301ea4176db732
EligiaOfficial <jasperebeli@hotmail.nl>
2023-10-08 11:42:52 +0200

    Basic Routing

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import CreateTicket from "./pages/CreateTicket";
+import Login from "./pages/Login";
 
 function App() {
 
   return (
     <>
-        <h1>Hello World</h1>
+        <Router>
+            <Routes>
+                <Route exact path={"/"} element={<Home/>}/>
+                <Route exact path={"/login"} element={<Login/>}/>
+                <Route exact path={"/create"} element={<CreateTicket/>}/>
+            </Routes>
+        </Router>
     </>
   )
 }
---
+++ ClientApp/src/pages/CreateTicket.tsx
@@ -0,0 +1,11 @@
+
+function CreateTicket() {
+
+    return (
+        <>
+            <h1>CreateTicket</h1>
+        </>
+    )
+}
+
+export default CreateTicket
