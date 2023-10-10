83eb8b198a21800d7f26712e68d7c0a453c30f5d
Ruben van Voorden <rubenvanvoorden@me.com>
2023-10-10 14:02:29 +0200

    Rename Data folder to Entities and move all classes defined inside of it under the Entities namespace.

---
+++ Entities/Machines.cs
@@ -0,0 +1,15 @@
+namespace Entities;
+
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+public class Machines
+{
+    [Key]
+    public int Mach_Id { get; set; }
+
+    public string Mach_Name { get; set; }
+
+    public string Mach_Type { get; set; }
+}
