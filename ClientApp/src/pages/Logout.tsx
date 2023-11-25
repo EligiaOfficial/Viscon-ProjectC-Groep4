420b84b0a12111bdb619f16fedda8c7755a6278a
EligiaOfficial <jasperebeli@hotmail.nl>
2023-11-25 12:31:49 +0100

    Use React Navigation instead of Vanilla JS
    For more consistency
    and avoid using vanillaJS while using the react framework

--- ClientApp/src/pages/Logout.tsx
+++ ClientApp/src/pages/Logout.tsx
@@ -1,6 +1,12 @@
-function Logout() {
-    localStorage.removeItem("token");
-    window.location.replace("/");
-}
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+    const navigateTo = useNavigate();
+    useEffect(() => {
+        localStorage.removeItem('token');
+        navigateTo('/');
+    }, [navigateTo]);
+};
 
 export default Logout;
