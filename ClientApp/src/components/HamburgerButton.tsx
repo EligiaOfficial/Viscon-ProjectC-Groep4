2ce46f817fe9ab41e0aef5df1b7e83bcaa91b7d0
Renjie Khor <r.khor@floow.nl>
2023-11-26 14:50:41 +0100

    Refactored sidebar

---
+++ ClientApp/src/components/HamburgerButton.tsx
@@ -0,0 +1,32 @@
+function HamburgerButton(props: any) {
+  return (
+    <div className="flex flex-row justify-end py-2">
+      <div
+        onClick={props.onclick}
+        className={`fill-black translate flex items-center justify-center min-w-[50px] cursor-pointer`}
+      >
+        <svg width="24" height="24">
+          <rect
+            className={`${
+              props.state ? "translate-x-[10px]" : ""
+            } translate duration-300`}
+            y="2"
+            width="24"
+            height="3"
+          ></rect>
+          <rect y="11" width="24" height="3"></rect>
+          <rect
+            className={`${
+              props.state ? "translate-x-[-10px]" : ""
+            } translate duration-300`}
+            y="20"
+            width="24"
+            height="3"
+          ></rect>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default HamburgerButton;
--- ClientApp/src/components/SideBar.tsx
+++ ClientApp/src/components/SideBar.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { getRole } from "../Endpoints/Jwt";
 import { useNavigate } from "react-router-dom";
+import SideBarItem from "./SideBarItem";
+import defaultIcon from "../assets/default-dashboard-icon.svg";
+import HamburgerButton from "./HamburgerButton";
 
 function SideBar() {
   const [menu, setMenu] = useState<boolean>(false);
@@ -11,124 +14,33 @@ function SideBar() {
     nav("/add");
   }
 
-  function navToLogout() {
-    nav("/logout");
-  }
-
   return (
     <div
       className={`${
         menu ? "w-[200px]" : "w-[50px]"
       } relative z-10 translate duration-300 flex flex-col gap-10 h-full bg-blue-500 overflow-hidden`}
     >
-      <div className="flex flex-row justify-end py-2">
-        <div
-          onClick={() => setMenu(!menu)}
-          className={`fill-black translate flex items-center justify-center min-w-[50px] cursor-pointer`}
-        >
-          <svg width="24" height="24">
-            <rect
-              className={`${
-                menu ? "translate-x-[10px]" : ""
-              } translate duration-300`}
-              y="2"
-              width="24"
-              height="3"
-            ></rect>
-            <rect y="11" width="24" height="3"></rect>
-            <rect
-              className={`${
-                menu ? "translate-x-[-10px]" : ""
-              } translate duration-300`}
-              y="20"
-              width="24"
-              height="3"
-            ></rect>
-          </svg>
-        </div>
-        <span></span>
-      </div>
+      <HamburgerButton onclick={() => setMenu(!menu)} state={menu} />
       <div className="flex flex-col">
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Dashboard
-          </span>
-        </div>
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Archive
-          </span>
-        </div>
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            New
-          </span>
-        </div>
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Critical
-          </span>
-        </div>
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Normal
-          </span>
-        </div>
+        <SideBarItem
+          title={"Dashboard"}
+          icon={defaultIcon}
+          onclick={() => nav("/dashboard")}
+        />
+        <SideBarItem
+          title={"Create Ticket"}
+          icon={defaultIcon}
+          onclick={() => nav("/create")}
+        />
+        <SideBarItem
+          title={"Add User"}
+          icon={defaultIcon}
+          onclick={() => nav("/add")}
+        />
+        <SideBarItem title={"Archive"} icon={defaultIcon} />
+        <SideBarItem title={"New"} icon={defaultIcon} />
+        <SideBarItem title={"Critical"} icon={defaultIcon} />
+        <SideBarItem title={"Normal"} icon={defaultIcon} />
       </div>
       <div className="mt-auto">
         {getRole(token) == UserRoles.ADMIN ||
@@ -137,59 +49,17 @@ function SideBar() {
             className="group flex flex-row justify-start py-2 cursor-pointer"
             onClick={navToAddAccount}
           >
-            <div
-              className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-            >
-              <svg
-                className="group-hover:fill-white fill-black translate duration-300"
-                width="24"
-                height="24"
-              >
-                <circle cx="12" cy="12" r="12" strokeWidth="4" />
-              </svg>
-            </div>
-            <span className="group-hover:text-white translate duration-300">
-              Add User
-            </span>
+            <SideBarItem title={"Add User"} icon={defaultIcon} />
           </div>
         ) : (
           <div />
         )}
-        <div className="group flex flex-row justify-start py-2 cursor-pointer">
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Settings
-          </span>
-        </div>
-        <div
-          className="group flex flex-row justify-start py-2 cursor-pointer"
-          onClick={navToLogout}
-        >
-          <div
-            className={`flex items-center justify-center min-w-[50px] cursor-pointer`}
-          >
-            <svg
-              className="group-hover:fill-white fill-black translate duration-300"
-              width="24"
-              height="24"
-            >
-              <circle cx="12" cy="12" r="12" strokeWidth="4" />
-            </svg>
-          </div>
-          <span className="group-hover:text-white translate duration-300">
-            Logout
-          </span>
-        </div>
+        <SideBarItem title={"Settings"} icon={defaultIcon} />
+        <SideBarItem
+          title={"Logout"}
+          icon={defaultIcon}
+          onclick={() => nav("/logout")}
+        />
       </div>
     </div>
   );
